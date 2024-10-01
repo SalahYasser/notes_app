cc95ced002f9a470cadbb216687f8e5d87384436
SalahYasser <salahy.allaithy@gmail.com>
2024-10-01 17:53:36 +0300

    display data part one

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -9,18 +9,13 @@ part 'notes_state.dart';
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
 
+  List<NoteModel>? notes;
+
   fetchAllNotes() async {
 
-    try {
-      var notesBox = Hive.box<NoteModel>(kNotesBox);
+    var notesBox = Hive.box<NoteModel>(kNotesBox);
 
-      List<NoteModel> notes = notesBox.values.toList();
+    notes = notesBox.values.toList();
 
-      emit(NotesSuccess(notes));
-
-    } catch (e) {
-
-      emit(NotesFailure(e.toString()));
-    }
   }
 }
