c1506fe1f966820c5ee6fe486853eac694269be0
SalahYasser <salahy.allaithy@gmail.com>
2024-10-01 19:29:04 +0300

    refresh notes list

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -16,6 +16,8 @@ class NotesCubit extends Cubit<NotesState> {
     var notesBox = Hive.box<NoteModel>(kNotesBox);
 
     notes = notesBox.values.toList();
+    
+    emit(NotesSuccess());
 
   }
 }
--- lib/cubits/notes_cubit/notes_state.dart
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -6,13 +6,8 @@ sealed class NotesState {}
 final class NotesInitial extends NotesState {}
 
 
+final class NotesSuccess extends NotesState {}
 
 
 // final class NotesLoading extends NotesState {}
 //
-// final class NotesSuccess extends NotesState {
-//
-//   final List<NoteModel> notes;
-//
-//   NotesSuccess(this.notes);
-// }
